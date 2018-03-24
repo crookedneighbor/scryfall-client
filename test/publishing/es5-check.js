66831fd0569168395e83d31e8ae69ed7f63763ca
blade <blade@crookedneighbor.com>
2018-03-23 22:44:27 -0500

    Fix publishing test

--- test/publishing/es5-check.js
+++ test/publishing/es5-check.js
@@ -1,12 +1,17 @@
 const browserify = require('browserify')
 const path = require('path')
 const fs = require('fs')
+const mkdirp = require('mkdirp')
 const checkES5 = require('check-ecmascript-version-compatibility')
 
-describe('built file', function () {
+describe('built file (be patient, this can take a while)', function () {
   before(function (done) {
+    let distLocation = path.resolve(__dirname, '..', '..', 'dist')
+
+    mkdirp.sync(distLocation)
+
     let name = 'mtg-cards'
-    this.path = path.resolve(__dirname, '..', '..', 'dist', `${name}.js`)
+    this.path = path.resolve(distLocation, `${name}.js`)
     let bundleFs = fs.createWriteStream(this.path)
     let b = browserify({standalone: name})
 
@@ -17,8 +22,8 @@ describe('built file', function () {
   })
 
   it('is es5 compliant', function (done) {
-    this.slow(22000)
-    this.timeout(25000)
+    this.slow(30000)
+    this.timeout(35000)
 
     checkES5(this.path, done)
   })
