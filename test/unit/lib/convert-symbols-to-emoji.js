81cb490a5bdeebcdf1d037851977d6a0a1db93a9
blade <blade@crookedneighbor.com>
2018-04-10 19:16:03 -0500

    DRY up emoji conversion code

---
+++ test/unit/lib/convert-symbols-to-emoji.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const convertSymbolsToEmoji = require('../../../lib/convert-symbols-to-emoji')
+
+describe('convertSymbolsToEmoji', function () {
+  describe('slack', function () {
+    it('replaces symbols with mana named emojis', function () {
+      expect(convertSymbolsToEmoji.slack('Foo {t} Bar')).to.equal('Foo :mana-t: Bar')
+    })
+
+    it('replaces hybrid mana symbols with mana named emojis', function () {
+      expect(convertSymbolsToEmoji.slack('Foo {u/b} Bar')).to.equal('Foo :mana-ub: Bar')
+    })
+
+    it('leaves text without symbols unchanged', function () {
+      expect(convertSymbolsToEmoji.slack('Foo Bar')).to.equal('Foo Bar')
+    })
+  })
+
+  describe('discord', function () {
+    it('replaces symbols with mana named emojis', function () {
+      expect(convertSymbolsToEmoji.discord('Foo {t} Bar')).to.equal('Foo :manat: Bar')
+    })
+
+    it('replaces hybrid mana symbols with mana named emojis', function () {
+      expect(convertSymbolsToEmoji.discord('Foo {u/b} Bar')).to.equal('Foo :manaub: Bar')
+    })
+
+    it('leaves text without symbols unchanged', function () {
+      expect(convertSymbolsToEmoji.discord('Foo Bar')).to.equal('Foo Bar')
+    })
+  })
+})
