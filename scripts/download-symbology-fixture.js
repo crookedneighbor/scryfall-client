a9ae3454130e6b377b9e5b360522a7566e75b0f7
Blade Barringer <blade@crookedneighbor.com>
2023-01-14 16:53:21 -0600

    refactor: remove workaround for erorr in Scryfall data
    closes #40

--- scripts/download-symbology-fixture.js
+++ scripts/download-symbology-fixture.js
@@ -13,52 +13,49 @@ const pathToFixture = path.resolve(
   "symbology.ts"
 );
 
-superagent
-  .get("https://api.scryfall.com/symbology?pretty=true")
-  .then((data) => {
-    const symbols = data.body.data.reduce((accum, data) => {
-      const char = data.symbol.replace(/[{}\/]/g, "");
+superagent.get("https://api.scryfall.com/symbology").then((data) => {
+  const symbols = data.body.data.reduce((accum, data) => {
+    const char = data.symbol.replace(/[{}\/]/g, "");
 
-      if (char in accum) {
-        throw new Error(`Unexpected duplicate symbol ${char}`);
-      }
-      // the .com -> .io replacement is a temporary measure while we wait for
-      // Scryfall to fix an error in their symbology endpoint that is displaying
-      // the wrong domain for the svg_uri property
-      const uri = data.svg_uri.replace("scryfall.com", "scryfall.io");
-      accum[char] = uri;
+    if (char in accum) {
+      throw new Error(`Unexpected duplicate symbol ${char}`);
+    }
+    const uri = data.svg_uri;
+    accum[char] = uri;
 
-      // if character is one of the Hybrid WUBRG Symbol
-      // or 2 Mana + WUBRG
-      // or WUBRG + WUBRG + Phyrexian
-      // then reverse the first two symbols in the key
-      // so that people don't have to remember the exact
-      // order that the symbols go in to use them
-      if (/^[WUBRG2][WUBRG](P)?$/.test(char)) {
-        const firstTwoCharactersInKeyReversed = char
-          .substring(0, 2)
-          .split("")
-          .reverse()
-          .join("");
-        const restOfKey = char.substring(2);
-        const commonMisspellingOfKey =
-          firstTwoCharactersInKeyReversed + restOfKey;
+    // if character is one of the Hybrid WUBRG Symbol
+    // or 2 Mana + WUBRG
+    // or WUBRG + WUBRG + Phyrexian
+    // then reverse the first two symbols in the key
+    // so that people don't have to remember the exact
+    // order that the symbols go in to use them
+    if (/^[WUBRG2][WUBRG](P)?$/.test(char)) {
+      const firstTwoCharactersInKeyReversed = char
+        .substring(0, 2)
+        .split("")
+        .reverse()
+        .join("");
+      const restOfKey = char.substring(2);
+      const commonMisspellingOfKey =
+        firstTwoCharactersInKeyReversed + restOfKey;
 
-        accum[commonMisspellingOfKey] = uri;
-      }
+      accum[commonMisspellingOfKey] = uri;
+    }
 
-      return accum;
-    }, {});
-    const existingModule = fs.readFileSync(pathToFixture, "utf8");
-    const module = `// auto-generated file, do not modify directly
+    return accum;
+  }, {});
+  const existingModule = fs.readFileSync(pathToFixture, "utf8");
+  const module = `// auto-generated file, do not modify directly
 // change /scripts/download-symbology-fixture.js if changes need to be made
 export default ${JSON.stringify(symbols, null, 2)} as Record<string, string>`;
 
-    if (existingModule !== module) {
-      fs.writeFileSync(pathToFixture, module, "utf8");
-      console.log(
-        "New symbolpogy data downloaded. Symbology fixture updated. Aborting pre-publish task so the new file can be committed."
-      );
-      process.exit(1);
-    }
-  });
+  if (existingModule !== module) {
+    fs.writeFileSync(pathToFixture, module, "utf8");
+    console.error(
+      "New symbology data downloaded. Symbology fixture updated. Aborting pre-publish task so the new file can be committed."
+    );
+    process.exit(1);
+  } else {
+    console.log("No changes to symbology data detected. Continuing.");
+  }
+});
