d65a06395c78c943f2af516e44b70b2fdb0592ff
Blade Barringer <blade@crookedneighbor.com>
2024-08-12 07:31:29 -0500

    fix: add back in json array

--- src/types/json.ts
+++ src/types/json.ts
@@ -1,5 +1,11 @@
 // from https://github.com/microsoft/TypeScript/issues/1897#issuecomment-338650717
-export type AnyJson = boolean | number | string | null | JsonArray | JsonMap;
+export type AnyJson =
+  | boolean
+  | number
+  | string
+  | null
+  | Array<AnyJson>
+  | JsonMap;
 export interface JsonMap {
   [key: string]: AnyJson;
 }
